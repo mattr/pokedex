6e2891c70936c6c3f8012628195f9dddb46eeba4
Matt Redmond <matt.redmond@katalyst.com.au>
2025-03-27 23:38:33 +1030

    Add cleanInput function and tests

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func cleanInput(input string) []string {
+	var list []string
+	for _, token := range strings.Split(input, " ") {
+		token = strings.TrimSpace(token)
+		if token != "" {
+			list = append(list, strings.ToLower(token))
+		}
+	}
+	return list
+}
 
 func main() {
 	fmt.Println("Hello, World!")
---
+++ main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_cleanInput(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		expected []string
+	}{
+		{
+			name:     "Test tokenize and strip whitespace",
+			args:     args{input: "  hello  world  "},
+			expected: []string{"hello", "world"},
+		},
+		{
+			name:     "Convert tokens to lower case",
+			args:     args{input: "Pikachu BULBASAUR CharMander"},
+			expected: []string{"pikachu", "bulbasaur", "charmander"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := cleanInput(tt.args.input); !reflect.DeepEqual(got, tt.expected) {
+				t.Errorf("cleanInput() = %v, want %v", got, tt.expected)
+			}
+		})
+	}
+}
